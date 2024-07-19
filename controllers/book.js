62c55fa2258a1da894c236df5fd297a8788b002b
thaleseqb <thalesquadros2@gmail.com>
2024-07-19 14:12:56 -0300

    fix the id and name validation bug

--- controllers/book.js
+++ controllers/book.js
@@ -28,9 +28,14 @@ function getById(req, res) {
 
 function postBook(req, res) {
     try {
-        addBook(req.body);
-        res.status(201);
-        res.send("Livro inserido com sucesso");
+        if (req.body.id && req.body.nome) {
+            addBook(req.body);
+            res.status(201);
+            res.send("Livro inserido com sucesso");
+        } else {
+            res.status(422);
+            res.send("Os campos id e nome são obrigatórios");
+        }
     } catch (error) {
         res.status(500);
         res.send(error.message);
